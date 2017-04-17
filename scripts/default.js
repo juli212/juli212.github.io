98406fa286c9aeb6c4190cdb87a95f8e402dce86
juli212 <jjgiraldi@gmail.com>
2017-04-17 00:49:26 -0400

    keep header, nav, aside inside body on scroll

--- scripts/default.js
+++ scripts/default.js
@@ -8,7 +8,6 @@ $(document).ready(function () {
      	if ($(this).hasClass('top-level')) {
         $(this).closest("li").addClass("highlighted");
       } else if ($(this).hasClass('second-level')) {
-      	// $(this).parent().parent().parent().addClass('highlighted')
       	$(this).closest('ul').closest('li').addClass('highlighted')
       } 
     }
