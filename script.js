5f2d49257f6e32fd01864f600a76ef6a01f0b2b0
juli212 <jjgiraldi@gmail.com>
2017-12-14 18:23:48 -0500

    update touchstart/click hover logic

---
+++ script.js
@@ -0,0 +1,35 @@
+$(document).ready(function() {
+
+  $('.project.taphover').on('touchstart mouseenter mouseleave click', function(e) {
+    $this = $(this).closest('.project')
+    event = e.type
+    hoverClasses = 'clickable hovered'
+    console.log(e.type)
+
+    if ( event == 'touchstart' && !$this.hasClass(hoverClasses) ) {
+      e.preventDefault()
+      $this.toggleClass(hoverClasses)
+      return false
+    } else if (event == 'touchstart' && !($(e.target).closest('a.taphover').length > 0 )) {
+      $this.toggleClass(hoverClasses)
+      return false
+    } else if (event == 'touchstart') {
+      return true
+    }
+
+    if (event == 'mouseenter') {
+      $this.addClass(hoverClasses)
+      return false
+    }
+    if (event == 'mouseleave') {
+      $this.removeClass(hoverClasses)
+      return false
+    }
+
+    if (event == 'click' && !$this.hasClass(hoverClasses) ) {
+      $this.toggleClass(hoverClasses)
+      return false
+    }
+  })
+
+})
